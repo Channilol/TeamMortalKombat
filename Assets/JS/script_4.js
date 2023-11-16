382eb4fe8f592e83bd36ae7ab9b89d57256a3c32
Omar Badr <omarbadr938@gmail.com>
2023-11-16 16:16:43 +0100

    before checkout

--- Assets/JS/script_4.js
+++ Assets/JS/script_4.js
@@ -10,111 +10,42 @@ paginaDomande.style.display = 'none'
 paginaRisultati.style.display = 'none'
 paginaFeedback.style.display = 'none'
 
-const difficulty = 'hard'
 
-async function getQuestions(difficulty) {
 
+///////
+let domande; // variabile globale
+
+const difficulty = 'hard';
+
+const getQuestions = async (difficulty) => {
     const response = await fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=${difficulty}`);
-    const domande = await response.json();
-    console.log(domande.results);
-}
+    const data = await response.json();
+    domande = data.results
+    return domande
+};
 
+const fetchQuestions = async () => { // Aspetta prima getQuestions prima di esguire fetchQuestions
+    await getQuestions(difficulty);
+};
 
-const domande = getQuestions(difficulty)
+// Esegui fetchQuestions
+fetchQuestions()
+    .then(() => {
+        // se funzione ok esegui console.log
+        console.log(domande)
 
-const indiceDomande = 0
+        creazioneDomanda(domande)
 
-const indiceRisposte = []
-
-let indiceDomandaCorrente = 0
-
-let punteggioTotale = 0
-
-// Status iniziale
-
-// TIMER
-
-let secondi = 60
-let timeoutId
-
-let numeriSecondi = document.querySelector('.testoSecondi')
-let donutTimer = document.querySelector('.animationTimer');
-
-const countdown = () => {
-    if (secondi > 0) {
-        numeriSecondi.innerHTML = secondi
-        secondi--
-        timeoutId = setTimeout(countdown, 1000)
-    } else {
-        clickRisposta()
-    }
-}
-
-const resetCountdown = () => {
-    clearTimeout(timeoutId)
-    secondi = 60
-    countdown()
-
-}
-
-const resetTimerSVG = () => {
-    // Rimuovi e riaggiungi l'elemento SVG per far ripartire l'animazione
-    const parent = donutTimer.parentNode;
-    const nextDonutTimer = donutTimer.cloneNode(true);
-    parent.replaceChild(nextDonutTimer, donutTimer);
-    donutTimer = nextDonutTimer;
-}
-
-
-// Script pagina 1
-
-paginaDomande.style.display = 'none'
-paginaRisultati.style.display = 'none'
-paginaFeedback.style.display = 'none'
-
-document.addEventListener('DOMContentLoaded', () => {
-    const bottoneProceed = document.querySelector('#bottoneProceed')
-    const checkBoxObbligatoria = document.querySelector('#checkBoxObbligatoria')
-
-    checkBoxObbligatoria.addEventListener('change', () => {
-        bottoneProceed.disabled = !checkBoxObbligatoria.checked
     })
-
-    bottoneProceed.addEventListener('click', (event) => {
-        if (checkBoxObbligatoria.checked) {
-            displayDue()
-            event.preventDefault();
-        }
-        else {
-            alert('Before proceeding to the test check all boxes')
-        }
-    })
-
-})
+    .catch((error) => {
+        // se errore esegui qui sotto
+        console.error('Errore di fetch', error);
+    });
 
 
-// Script pagina 2
+const creazioneDomanda = (domande) => {
 
-// Funzione generazione numeri random 0-3
+    console.log(domande.length)
 
-const rispostaRandomica = () => {
-    arrayNumeriEstratti = []
-    while (arrayNumeriEstratti.length < domande[indiceDomandaCorrente].risposte.length) {
-        numeroGenerato = Math.floor(Math.random() * domande[indiceDomandaCorrente].risposte.length);
-
-        if (!arrayNumeriEstratti.includes(numeroGenerato)) {
-            arrayNumeriEstratti.push(numeroGenerato);
-        }
-    }
-    return arrayNumeriEstratti
 }
 
-const divRisposte = document.querySelector('#divContenitoreBottoniDomande')
-
-
-
-const displayDue = (e) => {
-    paginaWelcome.style.display = 'none';
-    paginaDomande.style.display = 'block';
-    creazioneDomanda()
-}
